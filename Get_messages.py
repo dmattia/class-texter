55a9ee6b697aa20d6505376ee1e22671845e6da7
dmattia <dmattia@nd.edu>
2015-12-02 02:34:52 -0500

    Fixed small errors

--- Get_messages.py
+++ Get_messages.py
@@ -1,4 +1,5 @@
 from twilio.rest import TwilioRestClient
+from dbfunction import verify_number
 from time import sleep
 import os
 
@@ -10,26 +11,27 @@ def Send_Reply_verification(phone_number = None):
 	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 	
 	#messages = client.messages.list(from_ = phone_number).reverse() # Reversed to respond to the earliest messages first
-	messages = client.messages.list(from_ = phone_number).reverse()
+	messages = client.messages.list(from_ = phone_number)
 
 	did_receive = False
-	for message in messages:
-		body = message.body.lower().strip()
-		if body == "accept":
-			verify_number(phone_number)
-		 	client.messages.create(to= phone_number, from_=TWILIO_NUMBER, body="Thank you for the reply, your number has been verified! We will send you a message if a spot opens up in your course")
-		elif body == "deny":
-			remove_number(phone_number)
-			client.messages.create(to= phone_number, from_=TWILIO_NUMBER, body="Thank you for the reply, your number has been disconnected. You will no longer receive messages unless you resign up!")
+	if messages is not None:
+		for message in messages:
+			body = message.body.lower().strip()
+			if body == "accept":
+				verify_number(phone_number)
+			 	client.messages.create(to= phone_number, from_=TWILIO_NUMBER, body="Thank you for the reply, your number has been verified! We will send you a message if a spot opens up in your course")
+			elif body == "deny":
+				remove_number(phone_number)
+				client.messages.create(to= phone_number, from_=TWILIO_NUMBER, body="Thank you for the reply, your number has been disconnected. You will no longer receive messages unless you resign up!")
+				message_text = message.body.lower()
+				did_receive = True
+				#client.messages.delete(message.sid)
+			else:
+				client.messages.create(to= phone_number, from_=TWILIO_NUMBER, body="We cannot understand your response. Please reply 'yes' if you'd like to receive a text alert if a spot opens in your selected course, or 'stop' if you'd like to no longer receive messages")
+			
 			message_text = message.body.lower()
 			did_receive = True
 			client.messages.delete(message.sid)
-		else:
-			client.messages.create(to= phone_number, from_=TWILIO_NUMBER, body="We cannot understand your response. Please reply 'yes' if you'd like to receive a text alert if a spot opens in your selected course, or 'stop' if you'd like to no longer receive messages")
-		
-		message_text = message.body.lower()
-		did_receive = True
-		client.messages.delete(message.sid)
 	if did_receive:
 		return message_text
 	else:
@@ -70,7 +72,7 @@ def Check_For_Responses():
 	for message in messages:
 		if message.from_ != TWILIO_NUMBER:
 			Send_Reply_verification(message.from_)
-			client.messages.delete(message.sid)
+			#client.messages.delete(message.sid)
 
 
 
